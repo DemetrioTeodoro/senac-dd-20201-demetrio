964d328338336c6ac2f8278b28884555bed85668
Demetrio <demetrio.teodoro@gmail.com>
2020-04-17 14:13:05 -0300

    Exercicios Organizados

--- src/main/java/model/dao/Banco.java
+++ src/main/java/model/dao/Banco.java
@@ -70,7 +70,7 @@ public class Banco {
 	private static final String CONEXAO = "jdbc:mysql://localhost:3306/" + BANCODADOS
 			+ "?useTimezone=true&serverTimezone=UTC";
 	private static final String USER = "root";
-	private static final String PASSWORD = "";
+	private static final String PASSWORD = "1234";
 
 	public static final int CODIGO_RETORNO_ERRO_EXCLUSAO = 0;
 	public static final int CODIGO_RETORNO_SUCESSO_EXCLUSAO = 1;
